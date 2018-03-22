d9e8894289102ca7a19263e67b4d15a1e00b5cbe
sirjmkitavi <kitavimjoseph@gmail.com>
2018-03-22 03:38:28 +0300

    Change folder structure

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './src/components/App';
+import { AppRegistry } from 'react-native'
+import App from './src/App'
 
 AppRegistry.registerComponent('football', () => App);
